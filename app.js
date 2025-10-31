89d34fa5331568a185c84f82915be511c8503e0a
BQMWY <247518502@qq.com>
2025-10-31 10:19:12 +0800

    Test new VM CI/CD

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => {
   // 获取当前服务器时间并格式化
   const currentTime = new Date().toLocaleString('ja-JP', { timeZone: 'Asia/Tokyo' });
   res.send(`
-    <h1>自动化部署成功！</h1>
+    <h1>自动化部署(CI/CD) 复习成功！</h1>
     <p>This is a dynamic Node.js application.</p>
     <p>Current server time in Tokyo is: <strong>${currentTime}</strong></p>
   `);
